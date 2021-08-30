986a43c85c5fe28959c581e4d4a6e824a6144b01
Flavia <flavia.lima643@gmail.com>
2021-08-29 21:53:08 -0300

    Controle de Ponto de acesso.

---
+++ src/main/java/com/dio/live/model/TipoData.java
@@ -0,0 +1,13 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class TipoData {
+    private long id;
+    private String descricao;
+}
---
+++ src/main/java/com/dio/live/model/Usuario.java
@@ -0,0 +1,30 @@
+package com.dio.live.model;
+
+import lombok.*;
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Usuario {
+    private Long id;
+
+    @ManyToOne
+    private CategoriaUsuario categoriaUsuario;
+    private String nome;
+    @ManyToOne
+    private Empresa empresa;
+    @ManyToOne
+    private NivelAcesso nivelAcesso;
+    @ManyToOne
+    private JornadaTrabalho jornadaTrabalho;
+    private BigDecimal tolerancia;
+    private LocalDateTime inicioJornada;
+    private LocalDateTime finalJornada;
+}
